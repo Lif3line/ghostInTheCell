756b8b4e4cc442c4fecc4278cf09366b7d1e7095
Adam Hartwell <adamhartwell2@gmail.com>
2017-02-26 17:02:03 +0000

    Updated input getting matrices and distance comp

--- pythonBasic.py
+++ pythonBasic.py
@@ -3,54 +3,85 @@
 
 # Turn off irrelevant warning about constants vs variables
 # pylint: disable=C0103
+
 import sys
-import math
+from numpy import zeros
 
 factory_count = int(input())  # the number of factories
 link_count = int(input())  # the number of links between factories
+
+distances = zeros([factory_count, factory_count], dtype=int)
+
+# Get distances between pairs of factories
 for i in range(link_count):
-    factory_1, factory_2, distance = [int(j) for j in input().split()]
+    factory_1, factory_2, curDistance = [int(j) for j in input().split()]
+    distances[factory_1, factory_2] = curDistance
+
+# Mirror matrix since symetric along diagonal
+for i in range(factory_count):
+    for j in range(factory_count):
+        distances[j, i] = distances[i, j]
+
+print(link_count, file=sys.stderr)
+print(distances.shape, file=sys.stderr)
+print(distances, file=sys.stderr)
 
 # Game loop
 while True:
-    entity_count = int(input())  # Number of entities (factories and troops)
+    entityCount = int(input())  # Number of entities (factories and troops)
+    entityID = []
+    entityType = []
+    arg1 = []
+    arg2 = []
+    arg3 = []
+    arg4 = []
+    arg5 = []
+    for i in range(entityCount):
+        out1, out2, out3, out4, out5, out6, out7 = input().split()
+        entityID.append(int(out1))
+        entityType.append(out2)  # 'FACTORY' or 'TROOP'
+        arg1.append(int(out3))  # Owner (1 ally, -1 enemy, 0 neutral)
+        arg2.append(int(out4))  # F: # cyborgs, T: id of last factory visited
+        arg3.append(int(out5))  # F: production val, T: id of target factory
+        arg4.append(int(out6))  # F: # turns till prod., T: # cyborgs in troop
+        arg5.append(int(out7))  # F: N/A, T: # turns until destination
 
     curTargetID = -1
     curTargetProd = -1
     curTargetCyborgs = -1
+    curTargetDistance = 9999
 
     curSourceID = -1
     curSourceCyborgs = -1
 
     attackSize = 5  # Number of cyborgs to send to target
 
-    for i in range(entity_count):
-        entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 \
-            = input().split()
-
-        # 'FACTORY' or 'TROOP'
-        entity_id = int(entity_id)
-        arg_1 = int(arg_1)  # Owner (1 ally, -1 enemy, 0 neutral)
-        arg_2 = int(arg_2)  # F: # cyborgs, T: id of last factory visited
-        arg_3 = int(arg_3)  # F: production val, T: id of target factory
-        arg_4 = int(arg_4)  # F: # turns until prod., T: # cyborgs in troop
-        arg_5 = int(arg_5)  # F: N/A, T: # turns until destination
-
-        # Target the factory with highest production not already owned by us
-        if entity_type == "FACTORY":
-            if curTargetProd < arg_3 and arg_1 != 1:
-                curTargetID = entity_id
-                curTargetProd = arg_3
-                curTargetCyborgs = arg_2
-
+    # Compute our source
+    for i in range(entityCount):
         # Use our factory with the largest number of cyborgs as a source
-        if entity_type == "FACTORY":
-            if curSourceCyborgs < arg_2 and arg_1 == 1:
-                curSourceID = entity_id
-                curSourceCyborgs = arg_2
+        if entityType[i] == "FACTORY":
+            if arg1[i] == 1 and curSourceCyborgs < arg2[i]:
+                curSourceID = entityID[i]
+                curSourceCyborgs = arg2[i]
+
+    # Compute target
+    # Target the factory with highest production not already owned by us
+    # Subselect by distance
+    for i in range(entityCount):
+        if entityType[i] == "FACTORY":
+            if (arg1[i] != 1 and
+                    curTargetProd < arg3[i] and
+                    distances[curSourceID, entityID[i]] < curTargetDistance):
+                curTargetID = entityID[i]
+                curTargetProd = arg3[i]
+                curTargetCyborgs = arg2[i]
+                curTargetDistance = distances[curSourceID, entityID[i]]
 
     # Choose actions or print("Debug messages...", file=sys.stderr)
     if curSourceID != -1 and curTargetID != -1 and attackSize >= 0:
         print("MOVE {} {} {}".format(curSourceID, curTargetID, attackSize))
+        # print(";")
+        # print("MSG Rusb B!")
     else:
         print("WAIT")
+        print("No valid action found", file=sys.stderr)
