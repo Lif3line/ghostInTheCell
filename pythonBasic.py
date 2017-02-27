38bd904ec8580c244a2fb1289880ac09c2692d33
Adam Hartwell <adamhartwell2@gmail.com>
2017-02-27 20:39:55 +0000

    Upgraded getEntities into a function
    Get info from input on each entity and divide into arrays for factories, troops and bombs for easy searching

--- pythonBasic.py
+++ pythonBasic.py
@@ -9,7 +9,51 @@ from numpy import full
 
 
 def getFactoryValue(distance, production):
+    """Return a heuristic valuation of a target"""
     return production / distance
+
+
+def getEntities():
+    """Get all entities on the field"""
+    entityCount = int(input())  # Number of entities (factories and troops)
+    factoryInfo = [[] for i in range(5)]
+    troopInfo = [[] for i in range(6)]
+    bombInfo = [[] for i in range(5)]
+
+    factoryCount = 0
+    troopCount = 0
+    bombCount = 0
+
+    for _ in range(entityCount):
+        ID, entitityType, arg1, arg2, arg3, arg4, arg5 = input().split()
+
+        if entitityType == "FACTORY":
+            factoryInfo[0].append(int(ID))  # ID
+            factoryInfo[1].append(int(arg1))  # Owner
+            factoryInfo[2].append(int(arg2))  # # Cyborgs
+            factoryInfo[3].append(int(arg3))  # Production
+            factoryInfo[4].append(int(arg4))  # Turns until production
+            factoryCount += 1
+
+        elif entitityType == "TROOP":
+            troopInfo[0].append(int(ID))  # ID
+            troopInfo[1].append(int(arg1))  # Owner
+            troopInfo[2].append(int(arg2))  # Source factory ID
+            troopInfo[3].append(int(arg3))  # Target factory ID
+            troopInfo[4].append(int(arg4))  # Number of cyborgs
+            troopInfo[5].append(int(arg5))  # Time until arrival
+            troopCount += 1
+
+        elif entitityType == "BOMB":
+            bombInfo[0].append(int(ID))  # ID
+            bombInfo[1].append(int(arg1))  # Owner
+            bombInfo[2].append(int(arg2))  # Source factory ID
+            bombInfo[3].append(int(arg3))  # Target factory ID
+            bombInfo[4].append(int(arg4))  # Number of cyborgs
+            bombCount += 1
+
+    return (factoryInfo, troopInfo, bombInfo,
+            factoryCount, troopCount, bombCount)
 
 
 factory_count = int(input())  # the number of factories
@@ -32,85 +76,64 @@ for i in range(factory_count):
 
 # Game loop
 while True:
-    entityCount = int(input())  # Number of entities (factories and troops)
-    entityID = []
-    entityType = []
-    arg1 = []
-    arg2 = []
-    arg3 = []
-    arg4 = []
-    arg5 = []
-    for i in range(entityCount):
-        out1, out2, out3, out4, out5, out6, out7 = input().split()
-        entityID.append(int(out1))
-        entityType.append(out2)  # 'FACTORY' or 'TROOP'
-        arg1.append(int(out3))  # Owner (1 ally, -1 enemy, 0 neutral)
-        arg2.append(int(out4))  # F: # cyborgs, T: id of last factory visited
-        arg3.append(int(out5))  # F: production val, T: id of target factory
-        arg4.append(int(out6))  # F: # turns till prod., T: # cyborgs in troop
-        arg5.append(int(out7))  # F: N/A, T: # turns until destination
+    factories, troops, bombs, numFactories, numTroops, numBombs = getEntities()
 
     curTargetID = -1
     curTargetProd = -1
     curTargetCyborgs = -1
     curTargetValue = -1
     curTargetOwner = -2
+    curSourceID = -1
+    curSourceCyborgs = -1
+
     curSecTargetID = -1
     curSecTargetProd = -1
     curSecTargetDistance = 9999
-
-    curSourceID = -1
-    curSourceCyborgs = -1
     curSecSourceID = -1
     curSecSourceCyborgs = -1
-
-    attackSize = -1  # Number of cyborgs to send to target
     secAttackSize = 1
 
     # Compute our sources
-    for i in range(entityCount):
+    for i in range(numFactories):
         # Use our factory with the largest number of cyborgs as a source
-        if entityType[i] == "FACTORY":
-            if arg1[i] == 1 and curSourceCyborgs < arg2[i]:
-                curSecSourceID = curSourceID
-                curSecSourceCyborgs = curSourceCyborgs
+        if factories[1][i] == 1 and curSourceCyborgs < factories[2][i]:
+            curSecSourceID = curSourceID
+            curSecSourceCyborgs = curSourceCyborgs
 
-                curSourceID = entityID[i]
-                curSourceCyborgs = arg2[i]
+            curSourceID = factories[0][i]
+            curSourceCyborgs = factories[2][i]
 
     # Compute main target
     # Target the factory with highest production not already owned by us
     # Subselect by distance
-    for i in range(entityCount):
-        if entityType[i] == "FACTORY":
-            curValue = getFactoryValue(distances[curSourceID, entityID[i]],
-                                       arg3[i])
-            if (arg1[i] != 1 and
-                    entityID[i] != curSourceID and  # Prevents bug when backcap
-                    curValue > curTargetValue):
-                curTargetID = entityID[i]
-                curTargetProd = arg3[i]
-                curTargetCyborgs = arg2[i]
-                curTargetValue = curValue
-                curTargetOwner = arg1[i]
+    for i in range(numFactories):
+        curValue = getFactoryValue(distances[curSourceID, factories[0][i]],
+                                   factories[3][i])
+        if (factories[1][i] != 1 and
+                factories[0][i] != curSourceID and  # Prevents bug when backcap
+                curValue > curTargetValue):
+            curTargetID = factories[0][i]
+            curTargetProd = factories[3][i]
+            curTargetCyborgs = factories[2][i]
+            curTargetValue = curValue
+            curTargetOwner = factories[1][i]
 
     # Compute number of our cyborgs already heading for target
     cyborgsAttacking = 0
     cyborgsMovingToDefend = 0
     incomingCyborgs = 0
-    for i in range(entityCount):
-        if entityType[i] == "TROOP":
-            # Our cyborgs that are already attacking the target
-            if arg1[i] == 1 and arg3[i] == curTargetID:
-                cyborgsAttacking = cyborgsAttacking + arg4[i]
+    for i in range(numTroops):
+        # Our cyborgs that are already attacking the target
+        if troops[1][i] == 1 and troops[3][i] == curTargetID:
+            cyborgsAttacking = cyborgsAttacking + troops[4][i]
 
-            # Their cyborgs moving to defend
-            if arg1[i] == -1 and arg3[i] == curTargetID:
-                cyborgsMovingToDefend = cyborgsMovingToDefend + arg4[i]
+        # Their cyborgs moving to defend
+        if troops[1][i] == -1 and troops[3][i] == curTargetID:
+            cyborgsMovingToDefend = cyborgsMovingToDefend + troops[4][i]
 
-            # Cyborgs looking to attack our source
-            if arg1[i] == -1 and arg3[i] == curSourceID:
-                incomingCyborgs = incomingCyborgs + arg4[i]
+        # Cyborgs looking to attack our source
+        if troops[1][i] == -1 and troops[3][i] == curSourceID:
+            incomingCyborgs = incomingCyborgs + troops[4][i]
 
     cyborgsDefending = cyborgsMovingToDefend + curTargetCyborgs
 
@@ -127,12 +150,12 @@ while True:
 
     # Compute collateral target
     # Target the closest neutral factory with prod > 0
-    for i in range(entityCount):
-        if entityType[i] == "FACTORY":
-            if (arg1[i] == 0 and distances[curSecSourceID, entityID[i]] <
-                    curSecTargetDistance):
-                curSecTargetID = entityID[i]
-                curSecTargetDistance = distances[curSecSourceID, entityID[i]]
+    for i in range(numFactories):
+        if (factories[1][i] == 0 and
+                distances[curSecSourceID, factories[0][i]] <
+                curSecTargetDistance):
+            curSecTargetID = factories[0][i]
+            curSecTargetDistance = distances[curSecSourceID, factories[0][i]]
 
     mainAttackString = ""
     secAttackString = ""
